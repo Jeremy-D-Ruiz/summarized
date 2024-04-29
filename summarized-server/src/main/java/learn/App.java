4578889dcc2f4f4dc19259ce8ce7586cfebb5883
Jeremy <jeremy_ruiz6@yahoo.com>
2024-04-29 12:54:19 -0700

    added ollama dependency

---
+++ summarized-server/src/main/java/learn/App.java
@@ -0,0 +1,8 @@
+package learn;
+
+public class App {
+    private static final String LOCALHOST = "http://localhost:11434";
+    public static void main(String[] args) {
+
+    }
+}
---
+++ summarized/src/main/java/com/example/demo/SummarizedApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummarizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummarizedApplication.class, args);
+	}
+
+}

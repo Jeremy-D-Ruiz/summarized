2a2034b071ac0df77838c81b80f0969ba30d65d3
Jeremy <jeremy_ruiz6@yahoo.com>
2024-05-02 08:25:30 -0700

    Added User Model

---
+++ summarized-server/src/main/java/dev/assessment/summarized/model/User.java
@@ -0,0 +1,30 @@
+package dev.assessment.summarized.model;
+
+public class User {
+
+    int id;
+    String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public User(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    //also tracking passwords - every user has admin position/can update/create/view/delete history
+}
